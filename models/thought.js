7af0c08df116fd5d128bb88d0f3f29ba30b76431
josuepaniagua <josue.paniagua.jap@gmail.com>
2022-12-06 14:14:39 -0600

    updated thought.js

--- models/thought.js
+++ models/thought.js
@@ -21,10 +21,6 @@ const thoughtSchema = new Schema(
   }
 );
 
-thoughtSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
-});
-
 const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
