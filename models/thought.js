dac34aae6734ef5016d536254dbec767a438af0b
josuepaniagua <josue.paniagua.jap@gmail.com>
2022-12-06 13:54:42 -0600

    added thought and user to models

---
+++ models/thought.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
